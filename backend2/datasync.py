c182047493ccb8b779d44b7d52d84a05351e4134
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-09-27 22:57:43 -0400

    Add IO and refactor

--- backend2/datasync.py
+++ backend2/datasync.py
@@ -1,59 +1,63 @@
-# To-do:
-#   1. Write unit tests
-#   2. I/O
-
 # algorithm:
 # 1. pick one of the datacenters with most ids and call it master
 # 2. sync it with all of the others
 # 3. sync the rest of the datacenters with the master
 
-def sync(data_centers):
-    master = []
-    master_id = 0
-    for (index,center) in enumerate(data_centers):
-        if len(center) > len(master):
-            master = center
-            master_id = index
+class CommandLineInterface(object):
+    def read_input(self):
+        lines = []
+        n = int(raw_input())
+        for i in range(n):
+            line = raw_input()
+            lines.append(line)
+        return lines
 
-    for (index,center) in enumerate(data_centers):
-        diff = center - master
-        for item in diff:
-            print str(item) + ' ' + str(index+1) + ' '+ str(master_id+1)
-            master.add(item)
+    def get_datacenters_from_input(self, lines):
+        data_centers = []
+
+        for line in lines:
+            if not line.strip():
+                data_center = set()
+            else:
+                data_center = set(line.split(' '))
+
+            data_centers.append(data_center)
+
+        return data_centers
+
+class DataSync(object):
+
+    def sync(self, data_centers):
+        result = ""
+        master = []
+        master_id = 0
+        for (index,center) in enumerate(data_centers):
+            if len(center) > len(master):
+                master = center
+                master_id = index
+
+        for (index,center) in enumerate(data_centers):
+            diff = center - master
+            for item in diff:
+                result += str(item) + ' ' + str(index + 1) + ' ' + str(master_id + 1) + '\n'
+                master.add(item)
+
+        for (index,center) in enumerate(data_centers):
+            diff = master - center
+            for item in diff:
+                result += str(item) + ' ' + str(master_id + 1) + ' ' + str(index + 1) + '\n'
+
+                center.add(item)
+
+        result += 'done'
+        return result
 
 
-    for (index,center) in enumerate(data_centers):
-        diff = master - center
-        for item in diff:
-            print str(item) + ' ' + str(master_id+1) + ' '+ str(index+1)
-            center.add(item)
-
-    print 'done'
 
 if __name__ == '__main__':
-    # lines = [
-    #     '1 3 4',
-    #     '1 2 3',
-    #     '1 3',
-    #     '1 4 2'
-    # ]
-    # lines = [
-    #     '1 2',
-    #     '2 1'
-    # ]
+    cli = CommandLineInterface()
+    datasync = DataSync()
 
-    lines = [
-        '1 3 4 5 7',
-        '1 3',
-        '2',
-        ''
-    ]
-    data_centers = []
-    for line in lines:
-        if not line.strip():
-            data_center = set()
-        else:
-            data_center = set(line.split(' '))
-        data_centers.append(data_center)
-
-    sync(data_centers)
+    lines = cli.read_input()
+    datacenters = cli.get_datacenters_from_input(lines)
+    print datasync.sync(datacenters)
